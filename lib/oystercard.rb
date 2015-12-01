c6f4a979419e3352f4f4265a86c87f6780eaa52f
edwardkerry <edwardkerry@gmail.com>
2015-12-01 12:46:24 +0000

    Oystercard up to challege 12

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,13 +1,44 @@
 class Oystercard
 
-  attr_reader :balance
+  attr_reader :balance, :in_use, :starting_station, :end_station, :journey, :history
+
+  MAXIMUM_BALANCE = 90
+  MINIMUM_CHARGE = 1
 
   def initialize
     @balance = 0
+    @in_use = false
+    @starting_station = nil
+    @end_station = nil
+    @journey = {}
+    @history = []
   end
 
   def top_up(amount)
+    fail "Balance limit of #{MAXIMUM_BALANCE} reached" if balance + amount > MAXIMUM_BALANCE
     @balance += amount
+  end
+
+  def touch_in(station)
+    fail "Insufficient funds: #{balance}" if balance < MINIMUM_CHARGE
+    @starting_station = station
+  end
+
+  def touch_out(station)
+    deduct(MINIMUM_CHARGE)
+    @end_station = station
+    @journey[@starting_station] = @end_station
+    @starting_station = nil
+  end
+
+  def in_journey?
+    starting_station
+  end
+
+  private
+
+  def deduct(amount)
+    @balance -= amount
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -3,15 +3,98 @@ require 'oystercard'
 describe Oystercard do
 
   subject(:card) {described_class.new}
+  let(:entry_station) {double :station}
+  let(:exit_station) {double :station}
 
-  it 'should have a balance of 0' do
-    expect(subject.balance).to eq 0
+
+  describe 'card balance' do
+    it 'should have a balance of 0' do
+      expect(card.balance).to eq 0
+    end
+  end
+
+  describe 'journey history' do
+
+    it 'shout initialize with empty journey hash' do
+      expect(card.journey).to be_empty
+    end
+
+
+    #let(:journey){ {entry_station: entry_station, exit_station: exit_station}}
+    it 'should store journey in hash' do
+      card.top_up(20)
+      card.touch_in(:entry_station)
+      card.touch_out(:exit_station)
+      expect(card.journey).to include(:entry_station=>:exit_station)#journey
+    end
+
+    it 'should initialize with empty history array' do
+      expect(card.history).to be_empty
+    end
+
   end
 
   describe '#top_up' do
     it 'should increase the existing balance by an amount' do
-        expect{ card.top_up(1) }.to change {card.balance}.by 1
+        expect{card.top_up(1)}.to change {card.balance}.by 1
+    end
+
+    it 'should enforce a maximum balance on top up' do
+      card.top_up(Oystercard::MAXIMUM_BALANCE)
+      expect{card.top_up(1)}.
+      to raise_error "Balance limit of #{Oystercard::MAXIMUM_BALANCE} reached"
     end
   end
 
+  describe '#deduct' do
+
+    it 'should check that touch_out changes balance by amount' do
+      card.top_up(20)
+      expect{card.touch_out(:exit_station)}.
+      to change {card.balance}.by -Oystercard::MINIMUM_CHARGE
+    end
+  end
+
+  describe 'touch_in' do
+    it 'should raise an error if balance is under 1' do
+      expect{card.touch_in(:entry_station)}.
+      to raise_error "Insufficient funds: #{card.balance}"
+    end
+
+    it 'should remember the station' do
+      card.top_up(20)
+      card.touch_in(:entry_station)
+      expect(card.starting_station).to eq :entry_station
+    end
+
+  end
+
+  describe '#in_journey' do
+    it 'should change @in_use to true when card.touch_in' do
+      card.top_up(10)
+      card.touch_in(:entry_station)
+      expect(card).to be_in_journey
+    end
+
+    describe '#touch_out' do
+      it 'should change @in_use to true when card.touch_out' do
+        card.touch_out(:exit_station)
+        expect(card).not_to be_in_journey
+      end
+    end
+
+    it 'should define end station' do
+      card.touch_out(:exit_station)
+      expect(card.end_station).to eq :exit_station
+    end
+
+    it 'should forget the starting station' do
+      card.top_up(10)
+      card.touch_in(:entry_station)
+      card.touch_out(:exit_station)
+      expect(card.starting_station).to eq nil
+    end
+
+
+  end
 end
